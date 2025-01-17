c06101dcd9804b7bc6d7853944c999aa88a98d3c
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2025-01-17 07:49:21 +0000

    risolto tutto ma invalid host header

--- crime-report-app/src/app/app.module.ts
+++ crime-report-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { CrimeFormComponent } from './crime-form/crime-form.component';
 
@@ -10,10 +10,11 @@ import { CrimeFormComponent } from './crime-form/crime-form.component';
   declarations: [
     AppComponent,
     CrimeFormComponent,
-    HttpClientModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
